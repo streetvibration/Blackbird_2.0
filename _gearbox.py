f990d6ae6507e3b6ec9966e2c00c45f08697638c
joe <joe@vzkb.de>
2022-12-08 11:07:10 +0100

    Added some variables stuff & getting the authors of a repo

--- _gearbox.py
+++ _gearbox.py
@@ -1,49 +1,83 @@
 #!/usr/bin/python
 
 import sys
+import os
+import subprocess
 import json
 import os.path
 import g_vars
 
-class globVarsS():
+print('We are in the Module:', __name__)
 
+
+def initializeVars():
+    global var1
+    var1 = "Initiasl value"
+
+    global var2
+    var2 = False
+
+
+class globVarsS():
     # Basic calling values (arguments)
-    enviroment      = False
-    reponame        = False
+    enviroment = ""
+    reponame = ""
 
     # Values from the config file
-    path_svn_base   = False
-    path_travellers = False
+    path_svn_base = ""
+    path_travellers = ""
 
     # File definitions
-
-    # Path definitions
+    FILE_AUTHORS = "authors.txt"
 
     # Shortened for PathToTravellerReponame
-    pttr = False
+    pttr = ""
+    pttr_BOARDINGPASS = ""
+    pttr_AUTHORS = ""
+    psvn_REPO = ""
+
+
+def setGlobalVars():
+    globVarsS.psvn_REPO = globVarsS.path_svn_base + "/" + globVarsS.reponame
+    globVarsS.pttr = globVarsS.path_travellers + "/" + globVarsS.reponame
+    globVarsS.pttr_BOARDINGPASS = globVarsS.pttr + "/" + "boardingpass"
+    globVarsS.pttr_AUTHORS = globVarsS.pttr_BOARDINGPASS + "/" + globVarsS.FILE_AUTHORS
+
 
 def mapAuthors():
     print("Map aus GEARBOX")
     # Remove former files
-    #[ -e "${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}" ] && rm -f "${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}"
+    # [ -e "${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}" ] && rm -f "${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}"
     # As we do not have any standard mapping we just copy the file
-    #cp ${PATH_TRAVELLER_REPO_AUTHORS} ${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}
+    # cp ${PATH_TRAVELLER_REPO_AUTHORS} ${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}
 
 
+def getAuthors():
+    svn_filepath = "file://" + globVarsS.psvn_REPO
+    cmd = "svn log -q " + svn_filepath + " | grep '^r' | grep '|' | awk '{print $3}' | sort | uniq"
+    proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
 
-print('In the Module:', __name__)
+    # TODO change the open path to "globVarsS.pttr_AUTHORS" after we have all folders & file defined
+    f = open('authors.txt', 'a')  # open the file
+    f.seek(0)  # set the position back to the beginning
+    f.truncate()  # delete the content
+
+    for line in proc.stdout:
+        # print(line.strip().decode())
+        my_elem = line.strip().decode()
+        my_elem = my_elem + " " + "=" + " " + my_elem + " <" + my_elem + "@" + "mycompany.com>\n"
+        f.write(my_elem)  # add the line to the file
+        # print(my_elem)
+
+    f.close()  # close the file
+
 
 def readTheConfig(enviroment):
     """docstring"""
     from pathlib import Path
     print('\nRead the Config-File for enviroment: ', enviroment)
 
-    # declarring the vars
-    global PATH_CONFIG_FILE
-    global CONFIG_FILE
-
-    PATH_CONFIG_FILE = "specs"
-    CONFIG_FILE = PATH_CONFIG_FILE + "/config.json"
+    CONFIG_FILE = "specs/config.json"
 
     if os.path.exists(CONFIG_FILE):
         print(f'The file {CONFIG_FILE} exists')
@@ -53,8 +87,9 @@ def readTheConfig(enviroment):
     f = open(CONFIG_FILE)
     data = json.load(f)
 
-    g_vars.path_svn_base    = data[enviroment][0]['path_svn_base']
-    g_vars.path_travellers  = data[enviroment][0]['path_travellers']
+    # TODO - Check if we use this g_vars OR globVarsS
+    g_vars.path_svn_base = data[enviroment][0]['path_svn_base']
+    g_vars.path_travellers = data[enviroment][0]['path_travellers']
 
     globVarsS.path_svn_base = data[enviroment][0]['path_svn_base']
     globVarsS.path_travellers = data[enviroment][0]['path_travellers']
--- start_engine.py
+++ start_engine.py
@@ -8,15 +8,22 @@ import mechanics
 import svn
 import pprint
 
+#TODO How can we reuse the function mapAuthors from 2 different scripts with 2 different codes
+
 from _gearbox import mapAuthors
 from mechanics import mapAuthors
 
 mapAuthors()
 
 if __name__ == '__main__':
+
+    # TODO - Check if we use this g_vars OR globVarsS
     g_vars.initialize()
     print("SVN - After first run of init: ", g_vars.path_svn_base )
     print("Travellers - After first run of init: ", g_vars.path_travellers )
+
+    _gearbox.initializeVars()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--repo', required=True)
     parser.add_argument('-x', '--reset',        action='store_true')
@@ -30,7 +37,6 @@ if __name__ == '__main__':
     parser.add_argument('--analyze',            action='store_true')
     parser.add_argument('--keep-ruleset',       action='store_true')
     parser.add_argument('--trigger-initial',    action='store_true')
-    #parser.add_argument('--env',                action='store_true')
     parser.add_argument('--env', choices=['test', 'prod'])
     parser.add_argument('--gh-remote-name',     action='store_true')
 
@@ -44,15 +50,13 @@ if __name__ == '__main__':
         print("Running in :", _gearbox.globVarsS.enviroment, "enviroment")
 
     _gearbox.readTheConfig(_gearbox.globVarsS.enviroment)
-    #print("SVN - After reading the config:\t\t", g_vars.path_svn_base )
-    #print("Travellers - After reading the config:\t", g_vars.path_travellers )
 
-    print("\n The Script-Name & Arguments: \n")
-    print(__name__)
-    print(args)
+    #print("\n The Script-Name & Arguments: \n")
+    #print(__name__)
+    #print(args)
 
-    print("SVN-Base: ", _gearbox.globVarsS.path_svn_base)
-    print("Tavellers: ", _gearbox.globVarsS.path_travellers)
+    #print("SVN-Base: ", _gearbox.globVarsS.path_svn_base)
+    #print("Tavellers: ", _gearbox.globVarsS.path_travellers)
 
     if args.repo == "":
         print(f'No given repo name for argument -r')
@@ -60,6 +64,6 @@ if __name__ == '__main__':
         print(f'The repo ', args.repo, 'exists')
         _gearbox.globVarsS.reponame = args.repo
 
-    _gearbox.globVarsS.pttr = _gearbox.globVarsS.path_travellers + "/" + _gearbox.globVarsS.reponame
+    _gearbox.setGlobalVars()
 
-    print ("Path pttr:", _gearbox.globVarsS.pttr)
+    _gearbox.getAuthors()
