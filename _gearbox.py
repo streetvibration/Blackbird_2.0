45b4dc9e7d18b0950bb99fe19ced3c34bf4db8de
joe <joe@vzkb.de>
2022-12-12 14:02:26 +0100

    Added logging and runtime calculation

--- _gearbox.py
+++ _gearbox.py
@@ -2,13 +2,138 @@
 
 import sys
 import os
+import logging
 import subprocess
 import json
 import os.path
-import g_vars
+from logging import getLoggerClass, addLevelName, setLoggerClass, NOTSET
+from datetime import datetime
 
-print('We are in the Module:', __name__)
+# TODO - Tried to add a new log-level but something is not working
+# class SucessLogger(getLoggerClass()):
+#     def __init__(self, name, level=NOTSET):
+#         super().__init__(name, level)
+#
+#         addLevelName(5, "SUCCESS")
+#
+#     def success(self, msg, *args, **kwargs):
+#         if self.isEnabledFor(5):
+#             self._log(5, msg, args, **kwargs)
+#
+#
+# setLoggerClass(SucessLogger)
 
+def calcRuntime(sTime, eTime ):
+    tdelta = eTime - sTime
+    diff_in_minutes = 0
+    diff_in_seconds = round(tdelta.total_seconds(), 2)
+
+    if diff_in_seconds >= 60:
+        diff_in_minutes = round(diff_in_seconds / 60, 2)
+        dTime = str(diff_in_minutes)
+        dUnit = "Minutes"
+    else:
+        dTime = str(diff_in_seconds)
+        dUnit = "Seconds"
+
+    return dTime, dUnit
+
+
+def getLevelNames():
+    print("Levelnames: {}".format(", ".join(
+        logging.getLevelName(x)
+        for x in range(1, 101)
+        if not logging.getLevelName(x).startswith('Level'))))
+
+
+# Logging formatter supporting colorized output
+class LogFormatter(logging.Formatter):
+    COLOR_CODES = {
+        logging.CRITICAL: "\033[1;35m",  # bright/bold magenta
+        logging.ERROR: "\033[1;31m",  # bright/bold red
+        logging.WARNING: "\033[1;33m",  # bright/bold yellow
+        logging.INFO: "\033[0;37m",  # white / light gray
+        logging.DEBUG: "\033[1;30m",  # bright/bold black / dark gray
+        # logging.SUCCESS:  "\033[1;32m"  # bright/bold black / dark gray
+    }
+
+    RESET_CODE = "\033[0m"
+
+    def __init__(self, color, *args, **kwargs):
+        super(LogFormatter, self).__init__(*args, **kwargs)
+        self.color = color
+
+    def format(self, record, *args, **kwargs):
+        if (self.color == True and record.levelno in self.COLOR_CODES):
+            record.color_on = self.COLOR_CODES[record.levelno]
+            record.color_off = self.RESET_CODE
+        else:
+            record.color_on = ""
+            record.color_off = ""
+        return super(LogFormatter, self).format(record, *args, **kwargs)
+
+
+# Setup logging
+def setup_logging(console_log_output, console_log_level, console_log_color, logfile_date, logfile_file,
+                  logfile_log_level, logfile_log_color, log_line_template):
+    # Create logger
+    # For simplicity, we use the root logger, i.e. call 'logging.getLogger()'
+    # without name argument. This way we can simply use module methods for
+    # for logging throughout the script. An alternative would be exporting
+    # the logger, i.e. 'global logger; logger = logging.getLogger("<name>")'
+    logger = logging.getLogger()
+
+    # Set global log level to 'debug' (required for handler levels to work)
+    logger.setLevel(logging.DEBUG)
+
+    # Create console handler
+    console_log_output = console_log_output.lower()
+    if (console_log_output == "stdout"):
+        console_log_output = sys.stdout
+    elif (console_log_output == "stderr"):
+        console_log_output = sys.stderr
+    else:
+        print("Failed to set console output: invalid output: '%s'" % console_log_output)
+        return False
+    console_handler = logging.StreamHandler(console_log_output)
+
+    # Set console log level
+    try:
+        console_handler.setLevel(console_log_level.upper())  # only accepts uppercase level names
+    except:
+        print("Failed to set console log level: invalid level: '%s'" % console_log_level)
+        return False
+
+    # Create and set formatter, add console handler to logger
+    console_formatter = LogFormatter(fmt=log_line_template, color=console_log_color, datefmt='%Y-%m-%d %H:%M')
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    # Create log file handler
+    try:
+        logfile_handler = logging.FileHandler(logfile_file)
+    except Exception as exception:
+        print("Failed to set up log file: %s" % str(exception))
+        return False
+
+    # Set log file log level
+    try:
+        logfile_handler.setLevel(logfile_log_level.upper())  # only accepts uppercase level names
+    except:
+        print("Failed to set log file log level: invalid level: '%s'" % logfile_log_level)
+        return False
+
+    # Create and set formatter, add log file handler to logger
+    # logfile_formatter = LogFormatter(fmt=log_line_template, color=logfile_log_color)
+    logfile_formatter = LogFormatter(fmt=log_line_template, color=logfile_log_color, datefmt='%Y-%m-%d %H:%M')
+    logfile_handler.setFormatter(logfile_formatter)
+    logger.addHandler(logfile_handler)
+
+    # Success
+    return True
+
+
+# logging.info("We are in the Module:")
 
 def initializeVars():
     global var1
@@ -19,6 +144,8 @@ def initializeVars():
 
 
 class globVarsS():
+    # logging.info("Initiate the vars")
+
     # Basic calling values (arguments)
     enviroment = ""
     reponame = ""
@@ -37,6 +164,12 @@ class globVarsS():
     psvn_REPO = ""
 
 
+def checkIfSVN_RepoExists(path):
+    assert (os.path.exists(path)), "SVN-Repository not found"
+    logging.info("SVN-Repository at '" + globVarsS.psvn_REPO + "' exists.")
+    #isExisting = os.path.exists(path)
+
+
 def setGlobalVars():
     globVarsS.psvn_REPO = globVarsS.path_svn_base + "/" + globVarsS.reponame
     globVarsS.pttr = globVarsS.path_travellers + "/" + globVarsS.reponame
@@ -45,7 +178,8 @@ def setGlobalVars():
 
 
 def mapAuthors():
-    print("Map aus GEARBOX")
+    logging.info("Map aus GEARBOX")
+    logging.info("Mapping the authors")
     # Remove former files
     # [ -e "${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}" ] && rm -f "${PATH_TRAVELLER_REPO_AUTHORS_MAPPED}"
     # As we do not have any standard mapping we just copy the file
@@ -75,21 +209,19 @@ def getAuthors():
 def readTheConfig(enviroment):
     """docstring"""
     from pathlib import Path
-    print('\nRead the Config-File for enviroment: ', enviroment)
+    logging.info("Trying to read the Config-File for '" + globVarsS.enviroment + "' enviroment")
 
     CONFIG_FILE = "specs/config.json"
 
     if os.path.exists(CONFIG_FILE):
-        print(f'The file {CONFIG_FILE} exists')
+        logging.info("The config file at path '" + CONFIG_FILE + "' exists")
     else:
-        sys.exit(f'The file "{CONFIG_FILE}" does not exist')
+        sys.exit(f'The config file "{CONFIG_FILE}" does not exist')
 
     f = open(CONFIG_FILE)
     data = json.load(f)
 
-    # TODO - Check if we use this g_vars OR globVarsS
-    g_vars.path_svn_base = data[enviroment][0]['path_svn_base']
-    g_vars.path_travellers = data[enviroment][0]['path_travellers']
+    # Set the vars
 
     globVarsS.path_svn_base = data[enviroment][0]['path_svn_base']
     globVarsS.path_travellers = data[enviroment][0]['path_travellers']
--- start_engine.py
+++ start_engine.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
+"""
+This script controls the migration from a SVN-Repo to Git
+
+    Args:
+        -r | --repo (string): name of the SVN-repo to be migrated (reqired)
+        -x | --reset        : Resets the former data of a migration
+        -i | --info         : Gathers the info of the SVN-Repo
+        -e | --export       : Exporst a SVN-Repo into a bare Git-repo
+        -c | --clone        : Clones the bare repo into a git repo
+        -f | --filter       : Filters unwanted file-extensions out of the base repo
+        -p | --push         : Pushes the filtered git-repo to the remote repo
+        -b | --binaries     : Extract the filtered files into a flat filesystem
+        -a | --archive      : Extract one or more paths into a flat filesystem
+        --analize           : Analyzes a newly cloned repo
+        --keep-ruleset      : Prtect a handcraftet rules file from being overwritten
+        --env               : The desired environment to run th migration in ("test" is default
+        --gh-remote-name    : Provides a deviating remote name
+
+"""
+
+import logging
 import argparse
 import g_vars
 # import _gearbox as gb
@@ -7,20 +28,32 @@ import _gearbox
 import mechanics
 import svn
 import pprint
+import sys
+import os
+import subprocess
+from datetime import datetime
 
 #TODO How can we reuse the function mapAuthors from 2 different scripts with 2 different codes
 
 from _gearbox import mapAuthors
-from mechanics import mapAuthors
+
+# See all the logging level-names
+_gearbox.getLevelNames()
+
+# Initiate the logging procesdure
+script_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+
+if (not _gearbox.setup_logging(console_log_output="stdout", console_log_level="info", console_log_color=True,
+                               logfile_file=script_name + ".log", logfile_log_level="debug", logfile_log_color=False,
+                               logfile_date='%Y-%m-%d %H:%M',
+                               #log_line_template="%(color_on)s[%(created)d]  [%(module)s] %(lineno)d [%(threadName)s] [%(levelname)-8s] %(message)s%(color_off)s")):
+                               log_line_template="%(color_on)s %(asctime)s [%(filename)s:%(lineno)d] - %(levelname)s  %(message)s%(color_off)s")):
+    print("Failed to setup logging, aborting.")
 
 mapAuthors()
 
-if __name__ == '__main__':
 
-    # TODO - Check if we use this g_vars OR globVarsS
-    g_vars.initialize()
-    print("SVN - After first run of init: ", g_vars.path_svn_base )
-    print("Travellers - After first run of init: ", g_vars.path_travellers )
+if __name__ == '__main__':
 
     _gearbox.initializeVars()
 
@@ -42,28 +75,48 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if not args.env:
-        _gearbox.globVarsS.enviroment = "test"
-        print("No enviromet given- using 'test' as default")
-    else:
-        _gearbox.globVarsS.enviroment = args.env
-        print("Running in :", _gearbox.globVarsS.enviroment, "enviroment")
-
-    _gearbox.readTheConfig(_gearbox.globVarsS.enviroment)
-
-    #print("\n The Script-Name & Arguments: \n")
-    #print(__name__)
-    #print(args)
-
-    #print("SVN-Base: ", _gearbox.globVarsS.path_svn_base)
-    #print("Tavellers: ", _gearbox.globVarsS.path_travellers)
-
+    # Check if we have a given SVN-Repository
     if args.repo == "":
         print(f'No given repo name for argument -r')
-    else:
-        print(f'The repo ', args.repo, 'exists')
-        _gearbox.globVarsS.reponame = args.repo
+        sys.exit()
 
+    # Sdet the given reponame for further checks/proceeding
+    _gearbox.globVarsS.reponame = args.repo
+
+    # See if we have a given Envirinment - default "test"
+    if not args.env:
+        _gearbox.globVarsS.enviroment = "test"
+        logging.warning("No enviromet given- using 'test' as default")
+    else:
+        _gearbox.globVarsS.enviroment = args.env
+        logging.info("Running in :", _gearbox.globVarsS.enviroment, "enviroment")
+
+    # Read the config file - Exiting the script when the file does not exist
+    _gearbox.readTheConfig(_gearbox.globVarsS.enviroment)
+
+    # Checking/creating the travellers folder-structure
+    # _gearbox.createFMigStructure()
+
+    # Setting the vars after s successful read of the config file
     _gearbox.setGlobalVars()
+
+    # Check if the given SVN-Reponame is valid (existing folder)
+    try:
+        _gearbox.checkIfSVN_RepoExists(_gearbox.globVarsS.psvn_REPO)
+    except Exception as ex:
+        logging.exception('Caught an error')
+        sys.exit()
+
+    # Check if we have a given SVN-Repository
+    if args.reset:
+        logging.info("Resetting former migration data..")
+        # start time and end time
+        sTime = datetime.now()
+        subprocess.call('python3 ./flightplan/touchandgo.py', shell=True)
+        eTime = datetime.now()
+        a, b = _gearbox.calcRuntime(sTime, eTime)
+        print("Return time aus Calc: " + a + " " + b)
+    else:
+        logging.info("Resetting skipped")
 
     _gearbox.getAuthors()
